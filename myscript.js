b6d9070eea42d877489fa1dd04076b202b275a7f
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-14 20:05:40 +0100

    Palindroma-codice semplice

--- myscript.js
+++ myscript.js
@@ -1,7 +1,26 @@
 //Palidroma
     //Chiedere all’utente di inserire una parola 
-    //Creare una funzione per capire se la parola inserita è palindroma
+const input = prompt("Dimmi una parola e io controllerò se è palindroma");
 
+    //Creare una funzione per capire se la parola inserita è palindroma
+const parola = input.split('');
+console.log(parola);
+
+const parolaInversa = [];
+let j = 0;
+
+for (let i = parola.length - 1; i >= 0; i--) {
+    parolaInversa[j] = parola[i];
+    j++;
+}
+
+console.log(parolaInversa);
+
+if (parola == parolaInversa) {
+    alert(`la parola ${input} è palindroma`);
+} else {
+    alert(`la parola ${input} non è palindroma`);
+}
 
 //Pari e Dispari
     //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
